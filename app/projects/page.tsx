9014369c02bd023e0677b2a4636d4d09c2c02db6
Harriet Oteng <harriet.oteng@gmail.com>
2023-11-11 17:45:19 +0000

    Add onscroll event handler

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
-const dataSource = ['A', 'B']
+
+const dataSources = ['A', 'B', 'C', 'D'];
 
 const Projects = () => {
+  const [imgUrl, setImgUrl] = useState(dataSources[0]);
+
+  const handleScroll = (e: any) => {
+    const index = e.target.scrollTop / (e.target.scrollHeight / dataSources.length);
+    const selectedSource = dataSources[index];
+    if (selectedSource === imgUrl) {
+      return;
+    }
+    setImgUrl(selectedSource);
+  }
+
   return (
     <section className='h-full border-2 border-black'>
       <div className='border-2 h-1/2 flex items-center justify-center'>
-        <div className='max-h-full w-full max-w-[800px] aspect-video rounded-2xl border-2 border-red-500'></div>
+        <div className='max-h-full w-full max-w-[800px] aspect-video rounded-2xl border-2 border-red-500'>{imgUrl}</div>
       </div>
-      <div className='snap-mandatory snap-y overflow-auto border-2 h-1/2'>
+      <div onScroll={handleScroll} className='snap-mandatory snap-y overflow-auto border-2 h-1/2'>
         <div className='snap-always snap-center flex items-center justify-center font-bold text-3xl h-full border-2 border-red-600'>Metrics</div>
         <div className='snap-always snap-center flex items-center justify-center font-bold text-3xl h-full border-2 border-red-600'>Bookstore</div>
         <div className='snap-always snap-center flex items-center justify-center font-bold text-3xl h-full border-2 border-red-600'>Metrics</div>
