c0a279128582447332b20ddb120a11ab0d80a71e
Harriet Oteng <harriet.oteng@gmail.com>
2023-11-12 06:13:25 +0000

    Hide scrollbar

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -18,19 +18,18 @@ const Projects = () => {
   }
 
   return (
-    <section className='h-full border-2 border-black'>
+    <section className='h-full border-2 border-yellow-500'>
       <div className='border-2 h-1/2 flex items-center justify-center'>
         <div className='relative max-h-full w-full max-w-[800px] aspect-video rounded-2xl border-2 border-red-500'>
           <Image
             src={imgUrl}
             fill 
-            objectFit='cover'
-            alt='Picture on the soso project'
-            className='rounded-2xl'
+            alt='Picture of the project'
+            className='rounded-2xl shadow-lg object-cover'
           />
         </div>
       </div>
-      <div onScroll={handleScroll} className='snap-mandatory snap-y overflow-auto border-2 h-1/2'>
+      <div onScroll={handleScroll} className='snap-mandatory snap-y no-scrollbar overflow-y-scroll border-2 h-1/2'>
         <div className='snap-always snap-center flex items-center justify-center font-bold text-3xl h-full border-2 border-red-600'>Metrics</div>
         <div className='snap-always snap-center flex items-center justify-center font-bold text-3xl h-full border-2 border-red-600'>Bookstore</div>
         <div className='snap-always snap-center flex items-center justify-center font-bold text-3xl h-full border-2 border-red-600'>Metrics</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,20 @@ const config: Config = {
       // }
     },
   },
-  plugins: [],
+  plugins: [
+    // function ({ addUtilities }) {
+    //   const newUtilities = {
+    //     "no-scrollbar::-webkit-scrollbar": {
+    //       display: "none",
+    //     },
+    //     "no-scrollbar": {
+    //       "ms-overflow-style": "none",
+    //       "scrollbar-width": "none",
+    //     },
+    //   };
+
+    //   addUtilities(newUtilities)
+    // }
+  ],
 }
 export default config
