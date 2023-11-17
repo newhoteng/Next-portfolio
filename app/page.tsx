228ed508bee59b5d616d26e7a88437d5fa5933eb
Harriet Oteng <harriet.oteng@gmail.com>
2023-11-17 15:45:19 +0000

    Update desktop styles on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <section className="h-full flex items-center justify-center">
       <div className='text-center'>
-        <h1 className="text-4xl md:text-6xl font-bold text-secondary mb-2">Hello, my name is <br className='md:hidden' />Harriet Oteng.</h1>
-        <p className="mb-4 md:text-2xl">I&apos;m a Full-stack Software Developer and SVG animation enthusiast from Ghana.</p>
+        <h1 className="text-4xl md:text-5xl font-bold text-secondary mb-2">Hello, my name is <br />Harriet Oteng.</h1>
+        <p className="mb-4 md:text-xl">I&apos;m a Software Developer and SVG animation enthusiast from Ghana.</p>
         <div className='flex flex-col items-center justify-center gap-2 md:flex-row'>
           {links.map((link) => (
             <HomePageLink key={link.text} href={link.href} icon={link.icon} text={link.text} />
@@ -21,3 +21,5 @@ export default function Home() {
     </section>
   )
 }
+
+// className='md:hidden'
