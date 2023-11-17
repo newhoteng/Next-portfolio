cc0293208248de89ab643aac876c34ac9e0f59a1
Harriet Oteng <harriet.oteng@gmail.com>
2023-11-17 07:25:38 +0000

    Fix eslint error

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <section className="h-full flex items-center justify-center">
       <div className='text-center'>
         <h1 className="text-4xl md:text-6xl font-bold text-secondary mb-2">Hello, my name is <br className='md:hidden' />Harriet Oteng.</h1>
-        <p className="mb-4 md:text-2xl">I'm a Full-stack Software Developer and SVG animation enthusiast from Ghana.</p>
+        <p className="mb-4 md:text-2xl">I&apos;m a Full-stack Software Developer and SVG animation enthusiast from Ghana.</p>
         <div className='flex flex-col items-center justify-center gap-2 md:flex-row'>
           {links.map((link) => (
             <HomePageLink key={link.text} href={link.href} icon={link.icon} text={link.text} />
