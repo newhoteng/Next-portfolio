0c1dd0dba1c0450d62476ab97c73673e61c9aa24
Harriet Oteng <harriet.oteng@gmail.com>
2023-11-23 23:14:34 +0000

    Display desktop screenshots and add hero image

--- app/projects/metrics/page.tsx
+++ app/projects/metrics/page.tsx
@@ -5,11 +5,15 @@ import Image from 'next/image';
 const metrics = {
   name: 'metrics',
   description: 'A web app that fetches and displays air quality data for selected cities from a third party RESTful API. A click on a city tile on the homepage directs users to the details page which lists the concentration of the various pollutants. It has a colour indicator feature that shows an air icon in a different colour based on the current air quality index.',
-  screenshots: {
+  mobile: {
     'HOME PAGE': '/metrics/mobile-home.png',
     'POLLUTANTS PAGE': '/metrics/mobile-pollutants.png',
   },
-  techs: ['React', 'Redux', 'React Testing Library', 'React-Router', 'CSS Modules'],
+  desktop: {
+    'HOME PAGE': '/metrics/desktop-home.png',
+    'POLLUTANTS PAGE': '/metrics/desktop-pollutants.png',
+  },
+  techs: ['React', 'Redux', 'JavaScript'],
   live: 'https://metrics-59av.onrender.com/',
   source: 'https://github.com/newhoteng/Metrics',
 }
@@ -17,9 +21,11 @@ const metrics = {
 const Metrics = () => {
   return (
     <section className='border-2 flex flex-col gap-12'>
-      <div className='w-full bg-slate-400 aspect-[4/3] rounded-3xl border-2 border-green-600'></div>
+      <div className='flex items-center justify-center text-white w-full bg-[#fc5193] aspect-[4/3] rounded-3xl border-2 border-green-600'>
+        <h1>METRICS</h1>
+      </div>
       <div className='border-2 border-green-600'>
-        <h2 className='text-2xl font-semibold'>Metrics</h2>
+        <h2 className='text-2xl font-semibold'>METRICS</h2>
         <div className='border-2 flex gap-16'>
           <div>BUILT WITH</div>
           <div className='border-2 border-red-500 flex flex-col'>
@@ -39,11 +45,41 @@ const Metrics = () => {
         </div>
       </div>
       <div className='border-2 border-green-600'>
-        <h3 className='text-center font-bold'>Mobile Version</h3>
+        <h3 className='text-center'>HOMEPAGE</h3>
+        <div className='relative w-1/'>
+          <Image
+            src={metrics.desktop['HOME PAGE']}
+            // fill
+            width={0}
+            height={0}
+            sizes='100vw'
+            className="w-full auto object-contain"
+            alt=""
+            priority
+          />
+        </div>
+      </div>
+      <div className='border-2 border-green-600'>
+        <h3 className='text-center'>POLLUTANTS PAGE</h3>
+        <div className='relative w-1/'>
+          <Image
+            src={metrics.desktop['POLLUTANTS PAGE']}
+            // fill
+            width={0}
+            height={0}
+            sizes='100vw'
+            className="w-full auto object-contain"
+            alt=""
+            priority
+          />
+        </div>
+      </div>
+      <div className='border-2 border-green-600'>
+        <h3 className='text-center font-bold'>MOBILE VERSION</h3>
         <div className='border-2 border-red-500 flex items-cente justify-around'>
           <div className='relative w-1/3'>
             <Image
-              src={metrics.screenshots['HOME PAGE']}
+              src={metrics.mobile['HOME PAGE']}
               // fill
               width={0}
               height={0}
@@ -55,7 +91,7 @@ const Metrics = () => {
           </div>
           <div className='relative w-1/3'>
             <Image
-              src={metrics.screenshots['POLLUTANTS PAGE']}
+              src={metrics.mobile['POLLUTANTS PAGE']}
               // fill
               width={0}
               height={0}
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -9,9 +9,13 @@ import { Poppins } from 'next/font/google';
 //   subsets: ['latin'] 
 // });
 
+// const inter = Inter({ subsets: ['latin'], display: 'swap', adjustFontFallback: false})
+
 export const poppins = Poppins({ 
   weight: ['100','200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'] 
+  subsets: ['latin'] ,
+  display: 'swap',
+  adjustFontFallback: false
 });
 
 // export const oswald = Oswald({ 
