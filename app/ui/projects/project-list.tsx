0b2075c6afafadc3dc6768bc0f8c319a42ae925a
Harriet Oteng <harriet.oteng@gmail.com>
2023-11-29 13:19:18 +0000

    Modify banner on small screens to match large screens

--- app/ui/projects/project-list.tsx
+++ app/ui/projects/project-list.tsx
@@ -7,7 +7,7 @@ const ProjectList = ({ title, color, type, href } :
   }) => {
   return (
     <Link href={href}>
-      <div className={`shadow-around flex items-center bg-white p-4 rounded-md gap-4 hover:bg-${color}`}>
+      <div className='shadow-around flex items-center bg-white p-4 rounded-md gap-4'>
         <div style={{backgroundColor: color}} className='w-10 h-10 rounded'></div>
         <div className='grow flex items-center justify-between'>
           <h3 className='font-bold'>{title}</h3>
