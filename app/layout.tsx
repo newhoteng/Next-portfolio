d2db96148cb517f5b66aebc8b978936169739574
Harriet Oteng <harriet.oteng@gmail.com>
2023-12-08 17:18:36 +0000

    Test dark mode and update some hard coded color styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-// import { Inter } from 'next/font/google';
 import { poppins } from '@/app/ui/fonts';
 import './globals.css';
 import NavLinks from './ui/nav-links';
@@ -7,8 +6,6 @@ import Link from 'next/link';
 import Footer from './ui/footer';
 import { PersonalLogo } from '@/public/icons';
 import ColorThemeToggle from './ui/color-theme-toggle';
-
-// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Harriet Oteng',
@@ -22,11 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth'>
-      <body className={`${poppins.className} relative text-paragraph bg-[#fff9f3] max-w-screen-2xl my-0 mx-auto`}>
-        <header className="z-10 bg-[#fff9f3] fixed top-0 left-0 w-full h-[4rem] flex items-center justify-between">
+      <body className={`${poppins.className} relative text-paragraph bg-primary max-w-screen-2xl my-0 mx-auto`}>
+        <header className="z-10 bg-primary fixed top-0 left-0 w-full h-[4rem] flex items-center justify-between">
           <nav className="flex items-center justify-between max-w-screen-2xl w-full mx-auto px-4 md:px-12">
             <Link href="/">
-              <div className=" border border-accent rounded-lg w-12 md:w-14 text-secondary flex items-center justify-center">
+              <div className="w-12 md:w-14 text-secondary flex items-center justify-center">
                 <PersonalLogo />
               </div>
             </Link>
